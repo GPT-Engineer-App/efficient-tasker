6a6cf7c179cc11bc008655ac48d8e30b7b743d2d
Kristian Kyvik <kristianruizkyvik@gmail.com>
2024-03-21 14:02:08 +0000

    dasdas

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -30,7 +30,7 @@ const Index = () => {
   return (
     <Box maxWidth="600px" margin="auto" mt={8} p={4}>
       <Heading as="h1" size="xl" textAlign="center" mb={8}>
-        TH
+        THdasdasdsa
       </Heading>
       <form onSubmit={handleSubmit}>
         <HStack>
