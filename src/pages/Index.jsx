58d776b127ac1b0abe6caa7f88eafacb424877d0
Kristian Kyvik <kristianruizkyvik@gmail.com>
2024-03-21 12:22:03 +0000

    last commit

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -30,7 +30,7 @@ const Index = () => {
   return (
     <Box maxWidth="600px" margin="auto" mt={8} p={4}>
       <Heading as="h1" size="xl" textAlign="center" mb={8}>
-        sdaasd
+        THE NEW COPY
       </Heading>
       <form onSubmit={handleSubmit}>
         <HStack>
